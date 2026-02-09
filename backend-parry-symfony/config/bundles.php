87c3b5c0d47529cff3fbf14df839429e471cbbfb
FAUPIN Rémi <rfaupin@supplay.fr>
2026-02-09 11:02:28 +0000

    Ajout Login/Register jwt + rework nelmio documentation

--- backend-parry-symfony/config/bundles.php
+++ backend-parry-symfony/config/bundles.php
@@ -16,4 +16,5 @@ return [
     Symfony\Bundle\MercureBundle\MercureBundle::class => ['all' => true],
     Symfony\AI\AiBundle\AiBundle::class => ['all' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];
