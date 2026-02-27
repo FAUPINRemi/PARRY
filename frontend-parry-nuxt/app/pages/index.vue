041e5c33d22f9fc70398cfa3e3d95cd2444a41b1
Artur Moreira Machado <atfmrmc@gmail.com>
2026-02-27 15:59:08 +0100

    Base de homepage

--- frontend-parry-nuxt/app/pages/index.vue
+++ frontend-parry-nuxt/app/pages/index.vue
@@ -2,6 +2,44 @@
   const { emitEvent } = useTerminal();
 </script>
 
-<template>test</template>
+<template>
+  <div class="homepage">
+    <h1 class="homepage--title">PARRY</h1>
+    <pre class="homepage--ascii" aria-hidden="true">
+ooooooooo.         .o.       ooooooooo.   ooooooooo.   oooooo   oooo
+`888   `Y88.      .888.      `888   `Y88. `888   `Y88.  `888.   .8' 
+ 888   .d88'     .8"888.      888   .d88'  888   .d88'   `888. .8'  
+ 888ooo88P'     .8' `888.     888ooo88P'   888ooo88P'     `888.8'   
+ 888           .88ooo8888.    888`88b.     888`88b.        `888'    
+ 888          .8'     `888.   888  `88b.   888  `88b.       888     
+o888o        o88o     o8888o o888o  o888o o888o  o888o     o888o
+</pre
+    >
+    <div class="homepage--createRoom">
+      <h2>Créer un salon</h2>
+      <NuxtLink
+        class="gButton important"
+        @click="emitEvent({ message: 'TODO : Create game', type: 'info' })">
+        <Icon name="pixelarticons:plus" />
+        Créer un salon
+      </NuxtLink>
+    </div>
+    <form class="homepage--joinRoom">
+      <h2>Rejoindre le salon</h2>
+      <label> Code du salon:</label>
+      <div class="homepage--joinRoom--search">
+        <input type="text" placeholder="ABC123FR" />
+        <NuxtLink
+          class="gButton important"
+          @click="emitEvent({ message: 'TODO : Join game', type: 'info' })">
+          <Icon name="pixelarticons:search" />
+          Rejoindre
+        </NuxtLink>
+      </div>
+    </form>
+  </div>
+</template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+  @use "@/assets/style/page/homepage";
+</style>
