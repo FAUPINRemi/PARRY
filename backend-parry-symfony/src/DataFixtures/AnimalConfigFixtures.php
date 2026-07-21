021b44622bb32d6946456203df288e1a8b5c2536
Carl <Carl.heintz02@gmail.com>
2026-07-21 21:58:17 +0200

    feat: Ajout animaux animations

--- backend-parry-symfony/src/DataFixtures/AnimalConfigFixtures.php
+++ backend-parry-symfony/src/DataFixtures/AnimalConfigFixtures.php
@@ -8,32 +8,53 @@ use Doctrine\Persistence\ObjectManager;
 
 class AnimalConfigFixtures extends Fixture
 {
+    // Un jeu de sprites par animal "source" ; plusieurs alias peuvent partager
+    // la même source (voir ANIMALS) pour éviter de dessiner un visuel par animal.
+    private const SPRITE_SOURCES = [
+        'corbeau'  => ['/images/sprites/response/wait_corbeau.png', '/images/sprites/questions/question_corbeau.png', '/images/sprites/eliminations/dead_corbeau.png'],
+        'renard'   => ['/images/sprites/response/wait_renard.png', '/images/sprites/questions/question_renard.png', '/images/sprites/eliminations/dead_renard.png'],
+        'loup'     => ['/images/sprites/response/wait_loup.png', '/images/sprites/questions/question_loup.png', '/images/sprites/eliminations/dead_loup.png'],
+        'serpent'  => ['/images/sprites/response/wait_serpent.png', '/images/sprites/questions/question_serpent.png', '/images/sprites/eliminations/dead_serpent.png'],
+        'tigre'    => ['/images/sprites/response/wait_tigre.png', '/images/sprites/questions/question_tigre.png', '/images/sprites/eliminations/dead_tigre.png'],
+        'faucon'   => ['/images/sprites/response/wait_faucon.png', '/images/sprites/questions/question_faucon.png', '/images/sprites/eliminations/dead_faucon.png'],
+        'ours'     => ['/images/sprites/response/wait_ours.png', '/images/sprites/questions/question_ours.png', '/images/sprites/eliminations/dead_ours.png'],
+        'requin'   => ['/images/sprites/response/wait_requin.png', '/images/sprites/questions/question_requin.png', '/images/sprites/eliminations/dead_requin.png'],
+        'scorpion' => ['/images/sprites/response/wait_scorpion.png', '/images/sprites/questions/question_scorpion.png', '/images/sprites/eliminations/dead_scorpion.png'],
+        'hibou'    => ['/images/sprites/response/wait_hibou.png', '/images/sprites/questions/question_hibou.png', '/images/sprites/eliminations/dead_hibou.png'],
+        'baleine'  => ['/images/sprites/response/wait_baleine.png', '/images/sprites/questions/question_baleine.png', '/images/sprites/eliminations/dead_baleine.png'],
+    ];
+
+    // [alias, nom affiché, source des sprites]. Raton et Vautour n'ont pas
+    // encore de visuel dédié ni de source à partager : ils restent sur
+    // 'corbeau' en attendant.
     private const ANIMALS = [
-        ['corbeau', 'Corbeau', '/images/sprites/response/wait_corbeau.png', '/images/sprites/questions/question_corbeau.png', '/images/sprites/eliminations/dead_corbeau.png'],
-        ['renard', 'Renard', '/images/sprites/response/wait_renard.png', '/images/sprites/questions/question_renard.png', '/images/sprites/eliminations/dead_renard.png'],
-        ['loup', 'Loup', '/images/sprites/response/wait_loup.png', '/images/sprites/questions/question_loup.png', '/images/sprites/eliminations/dead_loup.png'],
-        ['serpent', 'Serpent', '/images/sprites/response/wait_serpent.png', '/images/sprites/questions/question_serpent.png', '/images/sprites/eliminations/dead_serpent.png'],
-        ['tigre', 'Tigre', '/images/sprites/response/wait_corbeau.png', '/images/sprites/questions/question_corbeau.png', '/images/sprites/eliminations/dead_corbeau.png'],
-        ['faucon', 'Faucon', '/images/sprites/response/wait_corbeau.png', '/images/sprites/questions/question_corbeau.png', '/images/sprites/eliminations/dead_corbeau.png'],
-        ['ours', 'Ours', '/images/sprites/response/wait_corbeau.png', '/images/sprites/questions/question_corbeau.png', '/images/sprites/eliminations/dead_corbeau.png'],
-        ['vipere', 'Vipère', '/images/sprites/response/wait_corbeau.png', '/images/sprites/questions/question_corbeau.png', '/images/sprites/eliminations/dead_corbeau.png'],
-        ['lynx', 'Lynx', '/images/sprites/response/wait_corbeau.png', '/images/sprites/questions/question_corbeau.png', '/images/sprites/eliminations/dead_corbeau.png'],
-        ['puma', 'Puma', '/images/sprites/response/wait_corbeau.png', '/images/sprites/questions/question_corbeau.png', '/images/sprites/eliminations/dead_corbeau.png'],
-        ['aigle', 'Aigle', '/images/sprites/response/wait_corbeau.png', '/images/sprites/questions/question_corbeau.png', '/images/sprites/eliminations/dead_corbeau.png'],
-        ['requin', 'Requin', '/images/sprites/response/wait_corbeau.png', '/images/sprites/questions/question_corbeau.png', '/images/sprites/eliminations/dead_corbeau.png'],
-        ['panthere', 'Panthère', '/images/sprites/response/wait_corbeau.png', '/images/sprites/questions/question_corbeau.png', '/images/sprites/eliminations/dead_corbeau.png'],
-        ['scorpion', 'Scorpion', '/images/sprites/response/wait_corbeau.png', '/images/sprites/questions/question_corbeau.png', '/images/sprites/eliminations/dead_corbeau.png'],
-        ['coyote', 'Coyote', '/images/sprites/response/wait_corbeau.png', '/images/sprites/questions/question_corbeau.png', '/images/sprites/eliminations/dead_corbeau.png'],
-        ['hibou', 'Hibou', '/images/sprites/response/wait_corbeau.png', '/images/sprites/questions/question_corbeau.png', '/images/sprites/eliminations/dead_corbeau.png'],
-        ['jaguar', 'Jaguar', '/images/sprites/response/wait_corbeau.png', '/images/sprites/questions/question_corbeau.png', '/images/sprites/eliminations/dead_corbeau.png'],
-        ['raton', 'Raton', '/images/sprites/response/wait_corbeau.png', '/images/sprites/questions/question_corbeau.png', '/images/sprites/eliminations/dead_corbeau.png'],
-        ['baleine', 'Baleine', '/images/sprites/response/wait_corbeau.png', '/images/sprites/questions/question_corbeau.png', '/images/sprites/eliminations/dead_corbeau.png'],
-        ['vautour', 'Vautour', '/images/sprites/response/wait_corbeau.png', '/images/sprites/questions/question_corbeau.png', '/images/sprites/eliminations/dead_corbeau.png'],
+        ['corbeau', 'Corbeau', 'corbeau'],
+        ['renard', 'Renard', 'renard'],
+        ['loup', 'Loup', 'loup'],
+        ['serpent', 'Serpent', 'serpent'],
+        ['vipere', 'Vipère', 'serpent'],
+        ['coyote', 'Coyote', 'renard'],
+        ['tigre', 'Tigre', 'tigre'],
+        ['faucon', 'Faucon', 'faucon'],
+        ['lynx', 'Lynx', 'tigre'],
+        ['puma', 'Puma', 'tigre'],
+        ['panthere', 'Panthère', 'tigre'],
+        ['jaguar', 'Jaguar', 'tigre'],
+        ['aigle', 'Aigle', 'faucon'],
+        ['ours', 'Ours', 'ours'],
+        ['requin', 'Requin', 'requin'],
+        ['scorpion', 'Scorpion', 'scorpion'],
+        ['hibou', 'Hibou', 'hibou'],
+        ['baleine', 'Baleine', 'baleine'],
+        ['raton', 'Raton', 'corbeau'],
+        ['vautour', 'Vautour', 'corbeau'],
     ];
 
     public function load(ObjectManager $manager): void
     {
-        foreach (self::ANIMALS as [$alias, $name, $response, $question, $elimination]) {
+        foreach (self::ANIMALS as [$alias, $name, $spriteSource]) {
+            [$response, $question, $elimination] = self::SPRITE_SOURCES[$spriteSource];
+
             $animal = new AnimalConfig();
             $animal->setAlias($alias);
             $animal->setAnimalName($name);
